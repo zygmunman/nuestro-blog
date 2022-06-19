5388687670ee71ce1e149b1bab4fcf505c0c0f99
zygmunman <jmart237@gmail.com>
2022-06-20 00:35:25 +0200

    Commit_5: Creación template MatrixAdmin

---
+++ resources/views/theme/back/app.blade.php
@@ -0,0 +1,96 @@
+
+<html dir="ltr" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Favicon icon -->
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset("assets/back/images/favicon.png")}}">
+        <title>@yield('titulo', 'Inicio') | Nuestro-Blog</title>
+        <!-- Custom CSS -->
+        <!-- Custom CSS -->
+        <link href="{{asset("assets/back/css/style.min.css")}}" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Main wrapper - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <div id="main-wrapper">
+            <!-- ============================================================== -->
+            <!-- Topbar header - style you can find in pages.scss -->
+            <!-- ============================================================== -->
+            @include('theme.back.top_header')
+            <!-- ============================================================== -->
+            <!-- End Topbar header -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Left Sidebar - style you can find in sidebar.scss  -->
+            <!-- ============================================================== -->
+            @include('theme.back.aside')
+            <!-- ============================================================== -->
+            <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Page wrapper  -->
+            <!-- ============================================================== -->
+            <div class="page-wrapper">
+                <!-- ============================================================== -->
+                <!-- Container fluid  -->
+                <!-- ============================================================== -->
+                <div class="container-fluid">
+                @yield('contenido')
+                    <h1>Hola, Bienvenid@s a la página de Inicio!!!</h1>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Container fluid  -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- footer -->
+                <!-- ============================================================== -->
+                @include('theme.back.footer')
+                <!-- ============================================================== -->
+                <!-- End footer -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Page wrapper  -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+
+        <!-- ============================================================== -->
+        <!-- All Jquery -->
+        <!-- ============================================================== -->
+        <script src="{{asset("assets/back/libs/jquery/dist/jquery.min.js")}}"></script>
+        <!-- Bootstrap tether Core JavaScript -->
+        <script src="{{asset("assets/back/libs/popper.js/dist/umd/popper.min.js")}}"></script>
+        <script src="{{asset("assets/back/libs/bootstrap/dist/js/bootstrap.min.js")}}"></script>
+        <script src="{{asset("assets/back/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js")}}"></script>
+        <script src="{{asset("assets/back/extra-libs/sparkline/sparkline.js")}}"></script>
+        <!--Wave Effects -->
+        <script src="{{asset("assets/back/js/waves.js")}}"></script>
+        <!--Menu sidebar -->
+        <script src="{{asset("assets/back/js/sidebarmenu.js")}}"></script>
+        <!--Custom JavaScript -->
+        <script src="{{asset("assets/back/js/custom.min.js")}}"></script>
+
+    </html
